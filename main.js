8a141f21b15134b3e35976813cdb30856c950abb
Murilo <murilopaulo051@gmail.com>
2025-06-03 20:54:50 -0300

    atualização 1.2

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 const btnalert = document.getElementById('btn-alert');
 const btnconfirm = document.getElementById('btn-confirm');
 const btnprompt = document.getElementById('btn-prompt');
+const n = document.getElementById('n');
+const mat = document.getElementById('mat');
 
-    btnalert.addEventListener('click', function() {
-        window.alert('Você clicou no teste do window alert');
-    });
+let n1 = 0;
+let n2 = 0;
 
-    btnconfirm.addEventListener('click', function() {
-        let resposta = window.confirm('Você clicou no teste do window confirm');
-        if (resposta) {
-            window.alert('Você clicou em OK');
-        } else {
-            window.alert('Você clicou em Cancelar');
-        }
-    });
+btnalert.addEventListener('click', function () {
+    window.alert('Você clicou no teste do window alert');
+});
 
-    btnprompt.addEventListener('click', function() {
-        let nome = window.prompt('Você clicou no teste do window prompt, digite qual é o seu nome');
-        if (nome) {
-            window.alert('Olá, ' + nome);
-        } else {
-            window.alert('Você clicou em Cancelar');
-        }
-    });
+btnconfirm.addEventListener('click', function () {
+    let resposta = window.confirm('Você clicou no teste do window confirm');
+    if (resposta) {
+        window.alert('Você clicou em OK');
+    } else {
+        window.alert('Você clicou em Cancelar');
+    }
+});
+
+btnprompt.addEventListener('click', function () {
+    let nome = window.prompt('Você clicou no teste do window prompt, digite qual é o seu nome');
+    if (nome) {
+        window.alert('Olá, ' + nome);
+    } else {
+        window.alert('Você clicou em Cancelar');
+    }
+});
+
+mat.addEventListener('click', function () {
+    n1 = window.prompt("Digite o primeiro número entre 1 e 10");
+    n2 = window.prompt("Digite o segundo número entre 1 e 10");
+    
+    n.style.display = 'block';
+    n.innerHTML = `A soma dos números é: ${parseInt(n1) + parseInt(n2)}`;
+});
