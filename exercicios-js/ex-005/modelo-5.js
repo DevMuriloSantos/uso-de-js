840aa24e746c7ade2fd8456a5f6729f447b357a7
Murilo <murilopaulo051@gmail.com>
2025-07-26 12:43:33 -0300

    atualização da validação de entrada e exibição do maior número

--- exercicios-js/ex-005/modelo-5.js
+++ exercicios-js/ex-005/modelo-5.js
@@ -1,6 +1,7 @@
 const add = document.getElementById('add')
 const list = document.getElementById('list')
 const atualizar = document.getElementById('finalizar')
+const resul = document.getElementById('resultado')
 const nadd = [];
 
 add.addEventListener('click', function (event) {
@@ -15,11 +16,18 @@ add.addEventListener('click', function (event) {
 
     const numero = Number(input);
 
-    for (let i = 0; i < list.children.length; i++){
+    for (let i = 0; i < list.children.length; i++) {
         if (numero == list.children[i].getAttribute('value')) {
             alert('Valor já existente na lista!')
+            document.getElementById('numero').value = '';
             return
         }
+    }
+
+    if (numero > 100) {
+        alert('Valor inválido!')
+        document.getElementById('numero').value = '';
+        return
     }
 
     const opt = document.createElement('option')
@@ -27,13 +35,25 @@ add.addEventListener('click', function (event) {
     opt.innerHTML = `Valor ${numero} adicionado.`
 
     list.appendChild(opt)
-    nadd.push(numero)
-    console.log(nadd)
+    nadd.push(numero);
+    nadd.sort(function (a, b) {
+        return a - b;
 
-    atualizar.addEventListener('click', function (event) {
-        event.preventDefault()
-
-        
-        console.log(nadd.sort())
     });
+
+    document.getElementById('numero').value = ''; // limpa o campo
+
+});
+
+atualizar.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const pos = nadd.length
+
+    const p = document.createElement('p')
+    p.setAttribute('class', 'msg')
+
+    p.innerHTML = `Ao todo, temos ${nadd.length} números cadastrados; <br>`
+    p.innerHTML += `O maior número informado foi o ${nadd[pos - 1]}; <br>`
+    resul.appendChild(p)
 });
