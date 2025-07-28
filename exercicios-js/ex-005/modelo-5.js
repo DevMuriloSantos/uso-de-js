39eb6b75a912dbbf1bb924d7ed4b737ab053fb4a
Murilo <murilopaulo051@gmail.com>
2025-07-28 01:21:59 -0300

    atualização 2.1

--- exercicios-js/ex-005/modelo-5.js
+++ exercicios-js/ex-005/modelo-5.js
@@ -2,6 +2,7 @@ const add = document.getElementById('add')
 const list = document.getElementById('list')
 const atualizar = document.getElementById('finalizar')
 const resul = document.getElementById('resultado')
+const p = document.querySelector('.msg')
 const nadd = [];
 
 add.addEventListener('click', function (event) {
@@ -16,7 +17,7 @@ add.addEventListener('click', function (event) {
 
     const numero = Number(input);
 
-    for (let i = 0; i < list.children.length; i++) {
+    for (let i in nadd) { // indicado apenas para arrays e objects
         if (numero == list.children[i].getAttribute('value')) {
             alert('Valor já existente na lista!')
             document.getElementById('numero').value = '';
@@ -36,24 +37,42 @@ add.addEventListener('click', function (event) {
 
     list.appendChild(opt)
     nadd.push(numero);
-    nadd.sort(function (a, b) {
+
+    nadd.sort(function (a, b) { // deixa em ordem crescente
         return a - b;
 
     });
 
     document.getElementById('numero').value = ''; // limpa o campo
+    p.innerHTML = ''
 
 });
 
-atualizar.addEventListener('click', function (event) {
+atualizar.addEventListener('click', (event) => { // forma diferente de declarar
     event.preventDefault();
+
+    if (list.children.length == 0) {
+        alert('[ERRO] Adicione valores antes de finalizar!')
+        return
+    }
+
+    p.innerHTML = ''
 
     const pos = nadd.length
 
-    const p = document.createElement('p')
-    p.setAttribute('class', 'msg')
+    let soma = 0;
+    let media = 0;
+
+    for (let i in nadd) {
+        soma += nadd[i]
+    }
+
+    media = soma / pos
 
     p.innerHTML = `Ao todo, temos ${nadd.length} números cadastrados; <br>`
-    p.innerHTML += `O maior número informado foi o ${nadd[pos - 1]}; <br>`
+    p.innerHTML += `O maior valor informado foi o ${nadd[pos - 1]}; <br>`
+    p.innerHTML += `O menor valor informado foi o ${nadd[0]}; <br>`
+    p.innerHTML += `Somando todos os valores, temos ${soma}; <br>`
+    p.innerHTML += `A média dos valores digitados é ${media};`
     resul.appendChild(p)
 });
