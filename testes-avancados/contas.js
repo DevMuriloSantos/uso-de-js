557f2ac6a384544756f08481e0813d707c0719d8
Murilo <murilopaulo051@gmail.com>
2025-08-02 03:39:40 -0300

    atualização 1.4

---
+++ testes-avancados/contas.js
@@ -0,0 +1,11 @@
+function soma(a, b) {
+    return a + b
+}
+
+function sub(a, b) {
+    return a - b
+}
+
+const person = { name: "Murilo", age: 20 }
+
+module.exports = { soma, sub, person } // exporta as funções e a variavel desejada
---
+++ testes-avancados/index.js
@@ -0,0 +1,5 @@
+const { soma, sub, person } = require('./contas') // importa tudo
+
+console.log(soma(2, 4))
+console.log(sub(5, 4))
+console.log(`Hello ${person.name}, you are ${person.age} years old`)
