390b5097f2fc7be81cb73509a9da7af0c6e8724e
Murilo <murilopaulo051@gmail.com>
2025-08-04 21:29:52 -0300

    atualização 1.4

--- testes-avancados/index.js
+++ testes-avancados/index.js
@@ -1,4 +1,4 @@
-const { soma, sub, person } = require('./contas') // importa tudo
+const { soma, sub, person } = require('./contas') // importa tudo do arquivo contas.js
 
 console.log(soma(2, 4))
 console.log(sub(5, 4))
